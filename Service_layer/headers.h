5c635e096701c9c0ed1bf25bcb4ba1b28209ede6
salehelmaraghy <33607434+salehelmaraghy@users.noreply.github.com>
2017-12-25 22:56:48 +0200

    Add files via upload

---
+++ Service_layer/headers.h
@@ -0,0 +1,16 @@
+/*
+ * headers.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: DELL
+ */
+
+#ifndef HEADERS_H_
+#define HEADERS_H_
+#include <avr/io.h>
+#include<util/delay.h>
+#include<avr/interrupt.h>
+typedef void(*fn_ptr)(void);
+
+
+#endif /* HEADERS_H_ */
